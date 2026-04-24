e2feb1169647eca0028b0a77d8f19887a3a51c3c
Banana_Niku87729 <130275367+Banana-Niku87729@users.noreply.github.com>
2026-04-24 17:43:15 +0900

    Update ytdlp_server.py

--- ytdlp_server.py
+++ ytdlp_server.py
@@ -39,6 +39,15 @@ def _extract(video_id: str) -> dict:
         "noplaylist": True,
         "skip_download": True,
         "cookiefile": "cookies.txt",
+        "extractor_args": {
+              "youtube": {
+                  "player_client": ["ios"],
+              }
+          },
+          # プロキシが使えない場合、これを入れると通ることがあります
+          "http_headers": {
+              "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1",
+          }
     }
     url = f"https://www.youtube.com/watch?v={video_id}"
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
